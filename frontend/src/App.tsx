f9e3dfb0b9695e99bb90123b4737ae9c2cf923c0
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-09-14 20:57:55 +0100

    initialize app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-white text-4xl bg-primary'>
+      <p className='font-sans'>hello a b c d f j h</p>
+      <p className='font-heading'>hello a b c d f j h</p>
+      <p className='font-body'>hello a b c d f j h</p>
+    </div>
+  )
+}
+
+export default App
